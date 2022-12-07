9fa6398bcf2a45b5694d77e851dc927acbdc39cc
olsch88 <olsch88@googlemail.com>
2022-12-07 22:42:50 +0100

    Updata day 5, works now

--- d5.py
+++ d5.py
@@ -1,27 +1,32 @@
 from typing import List
 
 
-def solve_part1(data: List[int]):
-
-    return 0
-
-
-def solve_part2(data: List[int]):
-
-    return 0
-
-
 def read_header(input_file: str):
     with open(input_file, "r") as file:
         header = []
         for line in file.readlines():
             if line != "\n":
-                print(len(line))
                 header.append(line)
             else:
                 break
-    print(header)
-    return header
+    # print(header)
+    return header[:-1]
+
+
+def get_stacks_from_header(header: List[str]):
+    no_of_stacks = int(len(header[0]) / 4)
+    # print(no_of_stacks)
+
+    stacks = [[] for x in range(no_of_stacks)]
+    for line in header:
+        print(line)
+        for i in range(no_of_stacks):
+            if line[1 + i * 4] != " ":
+                stacks[i].append(line[1 + i * 4])
+    # print(stacks)
+    for stack in stacks:
+        stack.reverse()
+    return stacks
 
 
 def read_body(input_file: str):
@@ -58,16 +63,55 @@ def reduce_body(body: List[str]):
     return reduced_body
 
 
+def process_step(stacks, step):
+    amount, from_stack, to_stack = step
+    for _ in range(amount):
+        temp = stacks[from_stack - 1][-1:]
+        stacks[from_stack - 1] = stacks[from_stack - 1][:-1]
+        stacks[to_stack - 1] += temp
+    # print(stacks)
+
+
+def process_step_orderly(stacks, step):
+    amount, from_stack, to_stack = step
+
+    temp = stacks[from_stack - 1][-amount:]
+    stacks[from_stack - 1] = stacks[from_stack - 1][:-amount]
+    stacks[to_stack - 1] += temp
+
+
+def solve_part1(stacks: List[str], steps: List[int]):
+    for step in steps:
+        process_step(stacks, step)
+
+    return "".join([s[-1] for s in stacks])
+
+
+def solve_part2(stacks: List[str], steps: List[int]):
+    for step in steps:
+        process_step_orderly(stacks, step)
+
+    return "".join([s[-1] for s in stacks])
+
+
 def main():
 
     data_clear = read_header("d5_input.txt")
+    stacks = get_stacks_from_header(data_clear)
 
-    # print("Solution Day 5, Part1:")
-    # print(solve_part1(data_clear))
-    # print("Solution Day 5, Part2:")
-    # print(solve_part2(data_clear))
+    steps = reduce_body(read_body("d5_input.txt"))
 
-    read_header("d5_sample.txt")
+    print("Solution Day 5, Part1:")
+    print(solve_part1(stacks, steps))
+
+    data_clear = read_header("d5_input.txt")
+    stacks = get_stacks_from_header(data_clear)
+
+    steps = reduce_body(read_body("d5_input.txt"))
+    print("Solution Day 5, Part2:")
+    print(solve_part2(stacks, steps))
+
+    # read_header("d5_sample.txt")
 
 
 if __name__ == "__main__":
