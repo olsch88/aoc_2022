40e586564ee39a6553c95da804fa147c02187088
olsch88 <95592115+olsch88@users.noreply.github.com>
2022-12-11 23:30:12 +0100

    clean up

--- d10.py
+++ d10.py
@@ -1,55 +1,41 @@
 from typing import List
 
 
-def get_signal_at_cycle(insturctions: List[str], cycles: List[int], x_only=False):
+def get_signal_at_cycle(insturctions: List[str]):
     """
     Return the Sum of cylce_no * signal (x) at each cycle_no in cycles
     """
-    if x_only:
-        signal = []
-    else:
-        signal = 0
+
+    signal = []
+
     cycle_counter = 0
     x = 1
     for command in insturctions:
         if command == "noop":
             cycle_counter += 1
-            if cycle_counter in cycles:
-                if x_only:
-                    signal.append(x)
-                else:
-                    signal += cycle_counter * x
-                # print(f"adding { cycle_counter * x} at cycle {cycle_counter}")
+            signal.append(x)
         else:  # if comand == "add ..."
             value = int(command.split()[1])
             for _ in range(2):
                 cycle_counter += 1
-                if cycle_counter in cycles:
-                    if x_only:
-                        signal.append(x)
-                    else:
-                        signal += cycle_counter * x
-                    # print(f"adding { cycle_counter * x} at cycle {cycle_counter}")
+                signal.append(x)
             x += value
 
     return signal
 
 
-def get_signal_mapping(insturctions: List[str]):
-    signals = get_signal_at_cycle(insturctions, list(range(1, 241)), True)
-    return signals
-
-
 def solve_part1(data: List[str]):
-    value = get_signal_at_cycle(data, [20, 60, 100, 140, 180, 220])
-    return value
+    signals = get_signal_at_cycle(data)
+    signal_strength_sum = 0
+    sample_cycles = [20, 60, 100, 140, 180, 220]
+    for cycle in sample_cycles:
+        signal_strength_sum += cycle * signals[cycle - 1]
+    return signal_strength_sum
 
 
 def solve_part2(data: List[str]):
-    cycle_positions = get_signal_mapping(data)  # index starts at 0
-    # print(cycle_positions)
+    cycle_positions = get_signal_at_cycle(data)  # index starts at 0
     crt = [["."] * 40 for _ in range(6)]
-    # print(crt)
     for i in range(240):
         if i % 40 in range(cycle_positions[i] - 1, cycle_positions[i] + 2):
             crt[i // 40][i % 40] = "#"
