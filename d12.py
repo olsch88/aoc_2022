1166c0be636e89c91f1834aaaab0106e80162013
olsch88 <95592115+olsch88@users.noreply.github.com>
2022-12-18 00:05:16 +0100

    Works now for part 1

--- d12.py
+++ d12.py
@@ -15,75 +15,86 @@ class Node:
     y: int = 0
     value: int = 0
     # neibours represents all adjcent fields that can be entered
-    neigbours: List["Node"] = field(default_factory=list, repr=False)
+    neighbours: List["Node"] = field(default_factory=list, repr=False)
     visited = False
-    distance = 0
+    distance = 999999
 
 
-def get_neigbours(maze: np.ndarray[Node], node: Node):
+def get_neighbours(maze: np.ndarray[Node], node: Node):
     """finding all possible destinations from a given node"""
     if not node.y - 1 < 0:
         neigh_val = maze[node.y - 1, node.x].value
         if neigh_val <= node.value + 1:
-            node.neigbours.append(maze[node.y - 1, node.x])
+            node.neighbours.append(maze[node.y - 1, node.x])
     if not node.y + 1 >= maze.shape[0]:
         neigh_val = maze[node.y + 1, node.x].value
         if neigh_val <= node.value + 1:
-            node.neigbours.append(maze[node.y + 1, node.x])
+            node.neighbours.append(maze[node.y + 1, node.x])
 
     if not node.x - 1 < 0:
         neigh_val = maze[node.y, node.x - 1].value
         if neigh_val <= node.value + 1:
-            node.neigbours.append(maze[node.y, node.x - 1])
+            node.neighbours.append(maze[node.y, node.x - 1])
     if not node.x + 1 >= maze.shape[1]:
         neigh_val = maze[node.y, node.x + 1].value
         if neigh_val <= node.value + 1:
-            node.neigbours.append(maze[node.y, node.x + 1])
+            node.neighbours.append(maze[node.y, node.x + 1])
 
 
-def traverse_node(maze: np.ndarray[Node], cur_node: Node, cur_length=0, lengths=None):
-    cur_node.visited = True
-    length = cur_length
-    cur_node.distance = cur_length
-    if cur_node.value == 27:  # End reached
-        lengths.append(cur_length)
-        # return cur_length
+# def traverse_node(cur_node: Node, cur_length=0, lengths=None):
+#     """this is dfs, not bfs"""
+#     cur_node.visited = True
+#     length = cur_length
+#     cur_node.distance = min(cur_length, cur_node.distance)
+#     if cur_node.value == 27:  # End reached
+#         lengths.append(cur_length)
+#         # return cur_length
 
-    for neigh in cur_node.neigbours:
-        if not neigh.visited:
-            length = traverse_node(
-                maze, neigh, cur_length=cur_length + 1, lengths=lengths
-            )
-    return  # length
+#     for neigh in cur_node.neighbours:  # [::-1]:
+#         if not neigh.visited:
+#             length = traverse_node(neigh, cur_length=cur_length + 1, lengths=lengths)
+#     return  # length
 
 
 # adaptiong an example found online:
 def bfs(
-    visited: List[Node], queue: List[Node], maze: np.ndarray[Node], start_node: Node
+    # maze: np.ndarray[Node],
+    start_node: Node,
+    target_node: Node,
 ):
-    """NOT YET IN USE"""
-    visited.append(start_node)
+
+    queue: List[Node] = []  # Initialize a queue
+    distance = 0
+    start_node.distance = distance
+    start_node.visited = True
     queue.append(start_node)
 
     while queue:
         next_node = queue.pop(0)
-        for neigh in next_node.neigbours:
-            visited.append(neigh)
-            queue.append(neigh)
+        distance = next_node.distance
+        # print(next_node)
+        if next_node == target_node:
+            return distance
+        for neigh in next_node.neighbours:
+            if not neigh.visited:
+                neigh.distance = distance + 1
+                neigh.visited = True
+                queue.append(neigh)
 
 
 def solve_part1(data: np.ndarray[Node]):
     for i in range(data.shape[0]):
         for j in range(data.shape[1]):
-            get_neigbours(data, data[i, j])
-    print(data)
-    data[0, 0].visited = True
-    posible_length = []
-    print(traverse_node(data, data[0, 0], 0, posible_length))
-    print(posible_length)
-    print(data)
-    print(data[2, 5].distance)
-    return 0
+            get_neighbours(data, data[i, j])
+            if data[i, j].value == heights["S"]:
+                start_node = data[i, j]
+                start_node.value = heights["a"]
+
+            if data[i, j].value == heights["E"]:
+                end_node = data[i, j]
+                end_node.value = heights["z"]
+
+    return bfs(start_node, end_node)
 
 
 def solve_part2(data: List[int]):
@@ -107,7 +118,7 @@ def read_data(input_file: str):
 
 def main():
 
-    data_clear = read_data("d12_sample.txt")
+    data_clear = read_data("d12_input.txt")
 
     print("Solution Day 12, Part1:")
     print(solve_part1(data_clear))
